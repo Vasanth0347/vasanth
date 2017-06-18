3dd3c2298008f7d41b125d6757c03710cd8436c3
Vasanth0347 <vasanth0347@gmail.com>
2017-06-18 15:09:08 +0530

    Create suma.c

---
+++ suma.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter the number 1 ::");
+scanf("%d",&a);
+printf("Enter the number 1b ::");
+scanf("%d",&b);
+c=a+b;
+printf("Addition of two number :",c);
+}
